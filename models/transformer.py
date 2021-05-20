2ccf088f53b854b583b52d514b60f45c9215d1f2
Teresa Pelinski <teresapelinski@gmail.com>
2021-05-20 18:26:50 +0200

    input and output layers now inside transformer class, renamed to TransformerGroove

--- models/transformer.py
+++ models/transformer.py
@@ -1,30 +1,57 @@
 import torch
 
-from utils import PositionalEncoding,get_tgt_mask
 from encoder import Encoder
 from decoder import Decoder
+from io_layers import InputLayer,OutputLayer
+
+from utils import get_tgt_mask
 
 
-class Transformer(torch.nn.Module):
-    def __init__(self, d_model, nhead, dim_feedforward, dropout, num_encoder_layers,
-                 num_decoder_layers, max_len):
-        super(Transformer, self).__init__()
+class GrooveTransformer(torch.nn.Module):
+    def __init__(self, d_model, embedding_size_src, embedding_size_tgt, nhead, dim_feedforward, dropout,
+                 num_encoder_layers, num_decoder_layers, max_len):
+        super(GrooveTransformer, self).__init__()
 
-        self.PositionalEncoder = PositionalEncoding(d_model, dropout, max_len)
+        self.d_model = d_model
+        self.embedding_size_src = embedding_size_src
+        self.embedding_size_tgt = embedding_size_tgt
+        self.nhead = nhead
+        self.dim_feedforward = dim_feedforward
+        self.dropout = dropout
+        self.max_len = max_len
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        self.max_len = max_len
+
+        self.InputLayerEncoder = InputLayer(embedding_size_src,d_model,dropout,max_len)
         self.Encoder = Encoder(d_model, nhead, dim_feedforward, dropout, num_encoder_layers)
+
+        self.InputLayerDecoder = InputLayer(embedding_size_tgt,d_model,dropout,max_len)
         self.Decoder = Decoder(d_model, nhead, dim_feedforward, dropout, num_decoder_layers)
+        self.OutputLayer = OutputLayer(embedding_size_tgt,d_model)
 
 
-    def forward(self, src, tgt, use_encoder=False, use_decoder=False):
-        x = self.PositionalEncoder(src)
+    def forward(self, src, tgt=None, only_encoder=False, only_decoder=False):
+        mask = get_tgt_mask(self.max_len)
+
+        if only_encoder:
+            x = self.InputLayerEncoder(src)
+            memory = self.Encoder(x)
+            out = self.OutputLayer(memory)
+            return out
+
+        if only_decoder:  # src is passed directly as memory
+            y = self.InputLayerDecoder(tgt)
+            memory = self.InputLayerEncoder(src)
+            out = self.Decoder(y, memory, tgt_mask=mask)
+            out = self.OutputLayer(out)
+            return out
+
+        # encoder-decoder implementation
+        x = self.InputLayerEncoder(src)
+        y = self.InputLayerDecoder(tgt)
         memory = self.Encoder(x)
-        mask = get_tgt_mask(tgt_len = tgt.shape[0])
-        out = self.Decoder(tgt, memory, tgt_mask=mask)
-
-        if use_encoder:
-            return memory
-
-        if use_decoder:
-            return self.Decoder(tgt, src, tgt_mask=mask)
+        out = self.Decoder(y, memory, tgt_mask=mask)
+        out = self.OutputLayer(out)
 
         return out
