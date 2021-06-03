16d389d7e906fe52ccd4e6d097356c22899fe9c8
Teresa Pelinski <teresapelinski@gmail.com>
2021-06-03 18:22:42 +0200

    fixed GrooveTransformerEncoder so that src and memory can have different sizes

--- models/transformer.py
+++ models/transformer.py
@@ -21,7 +21,6 @@ class GrooveTransformer(torch.nn.Module):
         self.max_len = max_len
         self.num_encoder_layers = num_encoder_layers
         self.num_decoder_layers = num_decoder_layers
-        self.max_len = max_len
         self.device = device
 
         self.InputLayerEncoder = InputLayer(embedding_size_src,d_model,dropout,max_len)
@@ -40,19 +39,19 @@ class GrooveTransformer(torch.nn.Module):
         y = self.InputLayerDecoder(tgt) # Nx32xd_model
         memory = self.Encoder(x)        # Nx32xd_model
         out = self.Decoder(y, memory, tgt_mask=mask) #Nx32xd_model
-        out = self.OutputLayer(out) #(Nx32xd_model,Nx32xd_model,Nx32xd_model)
+        out = self.OutputLayer(out) #(Nx32xembedding_size_src/3,Nx32xembedding_size_src/3,Nx32xembedding_size_src/3)
 
         return out
 
 
 class GrooveTransformerEncoder(torch.nn.Module):
-    def __init__(self, d_model, embedding_size_src, embedding_size_tgt, nhead, dim_feedforward, dropout,
+    def __init__(self, d_model, embedding_size_src, embedding_size_mem, nhead, dim_feedforward, dropout,
                  num_encoder_layers, num_decoder_layers, max_len, device):
         super(GrooveTransformerEncoder, self).__init__()
 
         self.d_model = d_model
         self.embedding_size_src = embedding_size_src
-        self.embedding_size_tgt = embedding_size_tgt
+        self.embedding_size_mem = embedding_size_mem
         self.nhead = nhead
         self.dim_feedforward = dim_feedforward
         self.dropout = dropout
@@ -64,7 +63,7 @@ class GrooveTransformerEncoder(torch.nn.Module):
 
         self.InputLayerEncoder = InputLayer(embedding_size_src,d_model,dropout,max_len)
         self.Encoder = Encoder(d_model, nhead, dim_feedforward, dropout, num_encoder_layers)
-        self.OutputLayer = OutputLayer(embedding_size_tgt,d_model)
+        self.OutputLayer = OutputLayer(embedding_size_mem,d_model)
 
     def forward(self, src):
         # src Nx32xembedding_size_src
@@ -72,6 +71,6 @@ class GrooveTransformerEncoder(torch.nn.Module):
 
         x = self.InputLayerEncoder(src) # Nx32xd_model
         memory = self.Encoder(x)        # Nx32xd_model
-        out = self.OutputLayer(memory)  #(Nx32xd_model,Nx32xd_model,Nx32xd_model)
+        out = self.OutputLayer(memory)  #(Nx32xembedding_size_mem/3,Nx32xembedding_size_mem/3,Nx32xembedding_size_mem/3)
 
         return out
