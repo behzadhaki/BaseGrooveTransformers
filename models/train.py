d1e398933f8412cbd337261e475c0ebcfd133005
marinaniet0 <marinanietogimenez@gmail.com>
2021-06-10 04:56:00 +0200

    Fixed model saving and loading, now working locally & on wandb

--- models/train.py
+++ models/train.py
@@ -1,8 +1,7 @@
-import sys
-from torch.utils.data import DataLoader
 import os
 import torch
 import wandb
+import re
 from models.transformer import GrooveTransformer
 
 
@@ -41,11 +40,10 @@ def calculate_loss(prediction, y, bce_fn, mse_fn):
     return total_loss, hit_accuracy.item(), hit_perplexity.item()
 
 
-def initialize_model(params, load_from_checkpoint=False):
-
+def initialize_model(params):
     model_params = params["model"]
     training_params = params["training"]
-    cp_info = params["cp_paths"]
+    load_model = params["load_model"]
 
     groove_transformer = GrooveTransformer(model_params['d_model'], model_params['embedding_size_src'],
                                            model_params['embedding_size_tgt'], model_params['n_heads'],
@@ -61,20 +59,44 @@ def initialize_model(params, load_from_checkpoint=False):
                                                 gamma=training_params['lr_scheduler_gamma'])
     epoch = 0
 
-    if load_from_checkpoint:
-        last_checkpoint = 0
-        for root, dirs, files in os.walk(cp_info['checkpoint_path']):
-            for name in files:
-                if name.startswith('transformer'):
-                    checkpoint_epoch = int(name.split('-')[-1])
-                    last_checkpoint = checkpoint_epoch if checkpoint_epoch > last_checkpoint else last_checkpoint
+    if load_model is not None:
 
-        if last_checkpoint > 0:
-            path = cp_info['checkpoint_save_str'].format(last_checkpoint)
-            checkpoint = torch.load(path)
-            groove_transformer.load_state_dict(checkpoint['model_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            epoch = checkpoint['epoch']
+        # If model was saved locally
+        if load_model['location'] == 'local':
+
+            last_checkpoint = 0
+            # From the file pattern, get the file extension of the saved model (in case there are other files in dir)
+            file_extension_pattern = re.compile(r'\w+')
+            file_ext = file_extension_pattern.findall(load_model['file_pattern'])[-1]
+
+            # Search for all continuous digits in the file name
+            ckpt_pattern = re.compile(r'\d+')
+            ckpt_filename = ""
+
+            # Iterate through files in directory, find last checkpoint
+            for root, dirs, files in os.walk(load_model['dir']):
+                for name in files:
+                    if name.endswith(file_ext):
+                        checkpoint_epoch = int(ckpt_pattern.findall(name)[-1])
+                        if checkpoint_epoch > last_checkpoint:
+                            last_checkpoint = checkpoint_epoch
+                            ckpt_filename = name
+
+            # Load latest checkpoint found
+            if last_checkpoint > 0:
+                path = os.path.join(load_model['dir'], ckpt_filename)
+                checkpoint = torch.load(path)
+
+        # If restoring from wandb
+        elif load_model['location'] == 'wandb':
+            model_file = wandb.restore(load_model['file_pattern'].format(load_model['run'],
+                                       load_model['epoch']), run_path=load_model['dir'])
+            checkpoint = torch.load(model_file.name)
+
+        groove_transformer.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        epoch = checkpoint['epoch']
 
     return groove_transformer, optimizer, scheduler, epoch
 
@@ -118,6 +140,8 @@ def train_loop(dataloader, groove_transformer, loss_fn, bce_fn, mse_fn, opt, sch
         save_path = os.path.join(wandb.run.dir, "saved_models")
         if not os.path.exists(save_path):
             os.makedirs(save_path)
+
+        save_filename = os.path.join(save_path, "transformer_run_{}_Epoch_{}.Model".format(wandb.run.id, epoch))
         torch.save({'epoch': epoch, 'model_state_dict': groove_transformer.state_dict(),
-                    'optimizer_state_dict': opt.state_dict(), 'loss': loss},
-                   os.path.join(save_path, "transformer_run_{}_Epoch_{}.Model".format(wandb.run.id, epoch)))
+                    'optimizer_state_dict': opt.state_dict(), 'scheduler_state_dict': scheduler.state_dict(),
+                    'loss': loss}, save_filename)
