Fix for loss calculation of test set

models/train.py
@@ -121,7 +121,7 @@ def initialize_model(params):
 
 
 def train_loop(dataloader, groove_transformer, loss_fn, bce_fn, mse_fn, opt, epoch, save, device,
-               encoder_only, test_inputs=None, h_loss_mult=1, v_loss_mult=1, o_loss_mult=1):
+               encoder_only, test_inputs=None, test_gt=None, h_loss_mult=1, v_loss_mult=1, o_loss_mult=1):
     size = len(dataloader.dataset)
     groove_transformer.train()  # train mode
     loss = 0
@@ -175,13 +175,13 @@ def train_loop(dataloader, groove_transformer, loss_fn, bce_fn, mse_fn, opt, epo
         torch.save({'epoch': epoch, 'model_state_dict': groove_transformer.state_dict(),
                     'optimizer_state_dict': opt.state_dict(), 'loss': loss}, save_filename)
 
-    if test_inputs is not None:
+    if test_inputs is not None and test_gt is not None:
         test_predictions_h, test_predictions_v, test_predictions_o = groove_transformer.predict(test_inputs,
                                                                                                 use_thres=True,
                                                                                                 thres=0.5)
         test_predictions = (test_predictions_h.float(), test_predictions_v.float(), test_predictions_o.float())
         test_loss, test_hits_accuracy, test_hits_perplexity, test_bce_h, test_mse_v, test_mse_o = \
-            loss_fn(test_predictions, test_inputs, bce_fn, mse_fn, h_loss_mult, v_loss_mult, o_loss_mult)
+            loss_fn(test_predictions, test_gt, bce_fn, mse_fn, h_loss_mult, v_loss_mult, o_loss_mult)
         wandb.log({'test_loss': test_loss.item(), 'test_hit_accuracy': test_hits_accuracy,
                    'test_hit_perplexity': test_hits_perplexity, 'test_hit_loss': test_bce_h.item(),
                    'test_velocity_loss': test_mse_v.item(), 'test_offset_loss': test_mse_o.item(), 'epoch': epoch})